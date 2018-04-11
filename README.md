aeb6b85d3e07d24108a84ea0a15886f00d3ca6a3
Fwrat <38071916+fwrat@users.noreply.github.com>
2018-04-11 13:13:19 +0300

    Files added
    all fiels added

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# meeting
-test
+## Meeting
+
+Prepare agenda, invite users and record minutes of a meeting
+
+#### License
+
+MIT
---
+++ meeting/meeting/doctype/lab/lab.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, Frappé and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Lab', {
+	refresh: function(frm) {
+
+	}
+});
